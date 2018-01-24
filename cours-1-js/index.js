33cb405cb6a047da77ea06939dbf6d525ced5d28
NicHub <nicolas.jeanmonod@gmail.com>
2018-01-25 00:03:42 +0100

    prepa cours 1

--- cours-1-js/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function main(source)
-{
-    var now = new Date();
-    console.log("fonction main " + now.getTime() +
-                "\nsource = " + source);
-}
-main("index.js");
--- cours-1-js/index2.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function main2()
-{
-    var now = new Date();
-    console.log("coucou de index 2 " + now.getTime());
-}
-main2();
